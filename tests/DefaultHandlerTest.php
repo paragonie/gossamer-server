ba401184c756a0f60d09e47d3a63495450691938
Paragon Initiative Enterprises <security@paragonie.com>
2020-01-14 19:18:26 -0500

    Add PHPUnit test

---
+++ tests/DefaultHandlerTest.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+namespace ParagonIE\GossamerServer\Tests;
+
+use GuzzleHttp\Psr7\ServerRequest;
+use ParagonIE\GossamerServer\Handlers\DefaultHandler;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Class DefaultHandlerTest
+ * @package ParagonIE\GossamerServer\Tests
+ */
+class DefaultHandlerTest extends TestCase
+{
+    public function testResponse()
+    {
+        $handler = new DefaultHandler([]);
+        $request = new ServerRequest('GET', '/', []);
+        $response = $handler($request);
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $body = (string) $response->getBody()->getContents();
+        $decoded = json_decode($body, true);
+        $this->assertIsArray($decoded);
+    }
+}

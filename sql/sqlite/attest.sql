25aeef8012184383a9b6125d44268d7fef1e4714
Paragon Initiative Enterprises <security@paragonie.com>
2021-01-02 19:36:15 -0500

    We're rolling this into libgossamer

--- sql/sqlite/attest.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE gossamer_package_release_attestations (
-    id INTEGER NOT NULL PRIMARY KEY,
-    release_id INTEGER REFERENCES gossamer_package_releases (id),
-    attestor INTEGER REFERENCES gossamer_providers (id),
-    attestation TEXT, -- 'reproduced', 'spot-check', 'code-review', 'sec-audit'
-    ledgerhash TEXT,
-    metadata TEXT
-);
